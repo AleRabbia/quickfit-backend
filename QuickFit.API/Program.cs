d931ad9f319136a1243f01d2d9dba48626006560
AleRabbia <alexisrabbia@gmail.com>
2026-08-31 20:10:22 -0300

    program.cs

--- QuickFit.API/Program.cs
+++ QuickFit.API/Program.cs
@@ -125,15 +125,18 @@ using (var scope = app.Services.CreateScope())
 }
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//
 
 app.UseCors("AllowReactApp");
 
-app.UseHttpsRedirection();
+if (!app.Environment.IsDevelopment())
+{
+    app.UseHttpsRedirection();
+}
 
 app.UseAuthentication();
 app.UseAuthorization();
